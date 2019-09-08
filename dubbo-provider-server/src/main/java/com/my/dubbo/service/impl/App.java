64bf05f7b0190bfdf45caf8611b6b106827525fd
rogan2 <wo177206204>
2019-09-08 17:48:34 +0800

    duboo基本应用

---
+++ dubbo-provider-server/src/main/java/com/my/dubbo/service/impl/App.java
@@ -0,0 +1,12 @@
+package com.my.dubbo.service.impl;
+
+
+import org.apache.dubbo.container.Main;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
